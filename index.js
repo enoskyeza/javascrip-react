37dfef757b3247256c80f0f6cf51f968f08e3d4a
enoskyeza <enoskyeza@gmail.com>
2023-12-02 23:33:45 +0300

    Add easy practise challenges

---
+++ index.js
@@ -0,0 +1,33 @@
+/* Easy Challenges
+
+Add Numbers:
+Write a function that takes two numbers as arguments and returns their sum.
+
+Even or Odd:
+Create a function that determines whether a given number is even or odd. Return 'even' or 'odd' accordingly.
+
+Reverse a String:
+Write a function that reverses a string (e.g., "hello" becomes "olleh").
+
+Find the Largest Number:
+Write a function that takes an array of numbers and returns the largest number in the array.
+
+Palindrome Checker:
+Create a function that checks whether a given word is a palindrome (reads the same forwards and backwards).
+
+Count Vowels:
+Write a function that counts the number of vowels in a given string.
+
+Fibonacci Sequence:
+Create a function to generate the Fibonacci sequence up to a certain number of terms.
+
+Find Factorial:
+Write a function that calculates the factorial of a given number.
+
+Remove Duplicates:
+Create a function that removes duplicate elements from an array.
+
+Title Case a Sentence:
+Write a function that converts the first letter of each word in a sentence to uppercase.
+
+*/
