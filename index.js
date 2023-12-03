758441ed64ebe8fa80821e9c6a2516d1aa15893c
enoskyeza <enoskyeza@gmail.com>
2023-12-03 21:10:57 +0300

    compete task 7

--- index.js
+++ index.js
@@ -88,4 +88,18 @@ function countVowels(str) {
     return count;
   }
 
-  console.log(countVowels("Hello World")); // Output: 3
+  console.log(countVowels("Hello World"));
+
+// 7 Fibinacci
+
+function generateFibonacci(n) {
+    let sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+      sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+    return sequence.slice(0, n);
+  }
+
+  // Example usage:
+  console.log(generateFibonacci(5));
+  console.log(generateFibonacci(8));
