136560b0f48d6515e22aa7921cbc115a6117ff43
enoskyeza <enoskyeza@gmail.com>
2023-12-06 23:32:07 +0300

    Implement a stack

--- index.js
+++ index.js
@@ -286,3 +286,36 @@ const originalObject = {
 
 const clonedObject = deepClone(originalObject);
 console.log(clonedObject);
+
+// Implement a stack
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    if (this.items.length === 0) {
+      return "Underflow";
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+}
+
+// Example usage:
+const stack = new Stack();
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack.peek()); // Output: 30
+console.log(stack.pop()); // Output: 30
+console.log(stack.pop()); // Output: 20
