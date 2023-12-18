9ac41e9b1e2ac781a30e113677ef73090454c8c1
enoskyeza <enoskyeza@gmail.com>
2023-12-18 23:01:42 +0300

    implemmment function

--- index.js
+++ index.js
@@ -493,3 +493,40 @@ class Node {
     this.right = null;
   }
 }
+
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let currentNode = this.root;
+    while (true) {
+      if (value === currentNode.value) {
+        return undefined; // Ignore duplicates (assuming no duplicates allowed)
+      }
+      if (value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return this;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return this;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+  // Additional methods (e.g., search, remove) can be implemented here
+}
