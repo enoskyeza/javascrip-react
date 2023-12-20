6436523c7372ab109999402404b1786cb31717ca
enoskyeza <enoskyeza@gmail.com>
2023-12-20 23:22:19 +0300

    Complete task 5.1

--- index.js
+++ index.js
@@ -628,7 +628,6 @@ function customMap(array, callback) {
   return result;
 }
 
-// Example usage:
 const numbers = [1, 2, 3, 4, 5];
 const squaredNumbers = customMap(numbers, (num) => num * num);
-console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
+console.log(squaredNumbers);
