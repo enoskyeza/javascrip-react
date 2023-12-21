948e7e6e315613b6d12296b65e58fc4380a625fc
enoskyeza <enoskyeza@gmail.com>
2023-12-21 22:40:18 +0300

    Add subscribe and unsubscribe methods for observable

--- index.js
+++ index.js
@@ -672,4 +672,9 @@ class Observable {
   subscribe(observer) {
     this.observers.push(observer);
   }
+
+  unsubscribe(observer) {
+    this.observers = this.observers.filter(obs => obs !== observer);
+  }
+
 }
