b380d9482889fbc82687a5807fb344eac7540afe
enoskyeza <enoskyeza@gmail.com>
2023-12-30 23:22:13 +0300

    create test cases

--- index.js
+++ index.js
@@ -811,3 +811,7 @@ function customPromiseAll(promises) {
     }
   });
 }
+
+const promise1 = new Promise((resolve) => setTimeout(() => resolve('First'), 1000));
+const promise2 = new Promise((resolve) => setTimeout(() => resolve('Second'), 2000));
+const promise3 = new Promise((resolve) => setTimeout(() => resolve('Third'), 3000));
